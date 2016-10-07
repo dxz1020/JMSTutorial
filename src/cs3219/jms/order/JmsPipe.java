8e48697de282a972d4ff9fe83762c0aca0204eb0
Duan Xu Zhou <dxz1020@hotmail.com>
2016-10-08 01:05:58 +0800

    JMS Pipe and Filter stubs

---
+++ src/cs3219/jms/order/JmsPipe.java
@@ -0,0 +1,37 @@
+package cs3219.jms.order;
+
+/**
+ * Matric 1:A0108453J
+ * Name   1:DUAN XUZHOU
+ * 
+ * Matric 2:A0111217Y
+ * Name   2:NG ZHI HUA
+ *
+ * This file implements a pipe that transfer messages using JMS.
+ */
+
+public class JmsPipe implements IPipe {
+    // your code here
+	public JmsPipe(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void write(Order s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Order read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+        
+}
