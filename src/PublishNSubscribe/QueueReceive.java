bb7c8e69adf30e502766d5cc439cfa6c4d99c3cf
Duan Xu Zhou <dxz1020@hotmail.com>
2016-10-07 13:15:10 +0800

    Stubs for PublishNSubscribe

---
+++ src/PublishNSubscribe/QueueReceive.java
@@ -0,0 +1,10 @@
+package PublishNSubscribe;
+
+public class QueueReceive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/PublishNSubscribe/QueueSend.java
@@ -0,0 +1,10 @@
+package PublishNSubscribe;
+
+public class QueueSend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
