72d23bb510a3c59218ec36123c5360bba69ac524
Duan Xu Zhou <dxz1020@hotmail.com>
2016-10-07 13:11:40 +0800

    Add stubs

---
+++ src/Point2Point/QueueReceive.java
@@ -0,0 +1,10 @@
+package Point2Point;
+
+public class QueueReceive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Point2Point/QueueSend.java
@@ -0,0 +1,10 @@
+package Point2Point;
+
+public class QueueSend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
