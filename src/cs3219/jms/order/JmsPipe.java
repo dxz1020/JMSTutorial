3bead76324cb6008d438746385116370d3c0771f
Zhi Hua <ngzhihua_@hotmail.com>
2016-10-09 00:25:18 +0800

    added working JmsPipe

--- src/cs3219/jms/order/JmsPipe.java
+++ src/cs3219/jms/order/JmsPipe.java
@@ -2,65 +2,120 @@ package cs3219.jms.order;
 
 import java.util.Properties;
 
+import javax.jms.JMSException;
+//import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+//import org.jboss.util.Sync;
+
 /**
- * Matric 1:A0108453J
- * Name   1:DUAN XUZHOU
+ * Matric 1:A0111217Y
+ * Name   1:Ng Zhi Hua
  * 
- * Matric 2:A0111217Y
- * Name   2:NG ZHI HUA
+ * Matric 2:A0108453J
+ * Name   2:Duan XuZhou
  *
  * This file implements a pipe that transfer messages using JMS.
  */
 
 public class JmsPipe implements IPipe {
-	// your code here
-	String customerName;
-	String productName;
-	private Queue queue;
+    
     private QueueConnectionFactory qconFactory;
     private QueueConnection qcon;
-	Boolean status;
-	public JmsPipe(String SimpleConnectionFactory, String SimpleQueue) throws Exception {
-		// TODO Auto-generated constructor stub
-		InitialContext ic = getInitialContext();
-		qconFactory = (QueueConnectionFactory) ic.lookup(SimpleConnectionFactory);
-		customerName=new String();
-		productName=new String();
-		status=true;
-	}
+    private QueueSession qsession;
+    private QueueSender qsender;
+    private QueueReceiver qreceiver;
+    private Queue queue;
+    private TextMessage msg;
+    
+    JmsPipe(String connectionName, String topicName){
+    	try {
+    		InitialContext ic = getInitialContext();
+			init(ic, topicName, connectionName);
+
+			System.out.println("Starting JMS...");
+    	} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+    }
 
 	@Override
 	public void write(Order s) {
+		try{
+		msg.setText(s.toString());
+		qsender.send(msg);
+		}catch(Exception e){
+			System.out.println("Error!: " + e.getMessage());
+		}
 		// TODO Auto-generated method stub
-		s.setCustomerName(customerName);
-		s.setProductName(productName);
-		status=false;
+		System.out.println("writing: " + s.toString());
+		
 	}
 
 	@Override
 	public Order read() {
-		// TODO Auto-generated method stub
-		Order order=new Order(customerName, productName);
-		if(status==true){
-			return order;
+		try {
+			TextMessage msg = (TextMessage)qreceiver.receive();
+			System.out.println("reading: " + Order.fromString(msg.getText()).toString());
+			return Order.fromString(msg.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
 		}
-		else{
-			return null;
-		}
+		
+		
+		return null;
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-		status=false;
+        try {
+        	qreceiver.close();
+            qsender.close();
+			qsession.close();
+	        qcon.close();
+		} catch (JMSException e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
 	}
+	
+    public void init(Context ctx, String queueName, String jmsFactory)
+            throws NamingException, JMSException {
+        qconFactory = (QueueConnectionFactory) ctx.lookup(jmsFactory);
+        qcon = qconFactory.createQueueConnection();
+        qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        queue = (Queue) ctx.lookup(queueName);
+        qsender = qsession.createSender(queue);
+        qreceiver = qsession.createReceiver(queue);
+        msg = qsession.createTextMessage();
+//        qreceiver.setMessageListener((MessageListener) this);
+        qcon.start();
+    }
+    
+    public void initReceiver(Context ctx, String queueName, String jmsFactory)
+            throws NamingException, JMSException {
+        qconFactory = (QueueConnectionFactory) ctx.lookup(jmsFactory);
+        qcon = qconFactory.createQueueConnection();
+        qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        queue = (Queue) ctx.lookup(queueName);
+        qreceiver = qsession.createReceiver(queue);
+//        qreceiver.setMessageListener(this);
+        qcon.start();
+    }
 	
     private static InitialContext getInitialContext()
             throws NamingException {
@@ -70,5 +125,5 @@ public class JmsPipe implements IPipe {
         props.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
         return new InitialContext(props);
     }
-
+    
 }
